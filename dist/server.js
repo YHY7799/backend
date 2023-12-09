147a14989301c5a48a4fd99141b1139e0c3b370b
Yahya <al.wlhan7799@gmail.com>
2023-12-09 15:30:08 +0300

    convert ts to js

--- dist/server.js
+++ dist/server.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
-const mongoose_1 = __importDefault(require("mongoose"));
-const validateEnv_1 = __importDefault(require("./util/validateEnv"));
+const app_1 = __importDefault(import("./app"));
+const mongoose_1 = __importDefault(import("mongoose"));
+const validateEnv_1 = __importDefault(import("./util/validateEnv"));
 const port = validateEnv_1.default.PORT;
 mongoose_1.default
     .connect(validateEnv_1.default.MONGO_CONNECTION_STRING)
